b2e14b427392b65424515c89d310361bb2b6bf7d
AbdulrasheedAdediran <abdrasheed1103@gmail.com>
2022-06-02 18:15:03 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+const App = () => {
+  return <main></main>;
+};
+
+export default App;
