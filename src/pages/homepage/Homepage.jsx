0fb8c87e5bd69701a9f45c46d5b694319325cd17
AbdulrasheedAdediran <abdrasheed1103@gmail.com>
2022-07-08 22:28:56 +0100

    markup

--- src/pages/homepage/Homepage.jsx
+++ src/pages/homepage/Homepage.jsx
@@ -1,7 +1,20 @@
+import { Link } from "react-router-dom";
 const Homepage = () => {
   return (
     <section>
-      <h2>Homepage</h2>
+      <div>
+        <p>So, you want to travel to</p>
+        <h1>Space</h1>
+        <p>
+          Let’s face it; if you want to go to space, you might as well genuinely
+          go to outer space and not hover kind of on the edge of it. Well sit
+          back, and relax because we’ll give you a truly out of this world
+          experience!
+        </p>
+      </div>
+      <div>
+        <Link to="/destination">Explore</Link>
+      </div>
     </section>
   );
 };
