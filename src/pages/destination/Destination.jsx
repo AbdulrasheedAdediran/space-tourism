5698fb4d414cc5f3e2ab7e3bbb02e76a09f0bd83
AbdulrasheedAdediran <abdrasheed1103@gmail.com>
2022-07-21 21:07:49 +0100

    destination

--- src/pages/destination/Destination.jsx
+++ src/pages/destination/Destination.jsx
@@ -1,25 +1,13 @@
-import { NavLink, Outlet, useParams } from "react-router-dom";
+import { NavLink, Outlet } from "react-router-dom";
 import data from "../../../data.json";
 const Destination = () => {
-  const { moon } = useParams();
   const destinations = data.destinations;
-  // const destination = destinations.find(
-  //   (destination) => destination.name === destinationName
-  // );
-  const {
-    name,
-    // images: { png, webp },
-    description,
-    distance,
-    travel,
-  } = destinations;
 
   return (
     <section>
       <h1>
         <span>00</span> <span>Pick your destination</span>
       </h1>
-      {/* <SingleDestination /> */}
       {destinations.map((destination) => (
         <nav key={destination.name}>
           <NavLink to={`/destination/${destination.name}`}>
@@ -28,13 +16,6 @@ const Destination = () => {
         </nav>
       ))}
       <Outlet />
-      {/* <li>{name}</li>
-      <li>
-        <img src={png} alt="" />
-      </li>
-      <li>{description}</li>
-      <li>{distance}</li>
-      <li>{travel}</li> */}
     </section>
   );
 };
