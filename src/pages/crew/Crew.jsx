4756d6acf8246a0cfbcb4b2706cee3001f78ecf0
AbdulrasheedAdediran <abdrasheed1103@gmail.com>
2022-07-24 19:39:00 +0100

    complete logic

--- src/pages/crew/Crew.jsx
+++ src/pages/crew/Crew.jsx
@@ -1,14 +1,13 @@
 import { useState } from "react";
 import data from "../../../data.json";
-
+import "./Crew.scss";
 const Crew = () => {
+  const [activeIndex, setActiveIndex] = useState(0);
   const crew = data.crew;
-  const [index, setIndex] = useState(0);
-  const [activeButton, setActiveButton] = useState("moonBtn");
-  const activeCrewMember = crew[index];
+  const activeCrewMember = crew[activeIndex];
   const { name, images, role, bio } = activeCrewMember;
   return (
-    <section>
+    <section className="crew">
       <h2>
         <span>02</span> <span>Meet your crew</span>
       </h2>
@@ -17,12 +16,32 @@ const Crew = () => {
         <img src={images.webp} alt="" />
       </div>
 
-      <nav>
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-      </nav>
+      <article>
+        <nav>
+          <button
+            className={`link ${activeIndex === 0 ? "active" : ""}`}
+            onClick={() => setActiveIndex(0)}
+          ></button>
+          <button
+            className={`link ${activeIndex === 1 ? "active" : ""}`}
+            onClick={() => setActiveIndex(1)}
+          ></button>
+          <button
+            className={`link ${activeIndex === 2 ? "active" : ""}`}
+            onClick={() => setActiveIndex(2)}
+          ></button>
+          <button
+            className={`link ${activeIndex === 3 ? "active" : ""}`}
+            onClick={() => setActiveIndex(3)}
+          ></button>
+        </nav>
+
+        <div className="crew-member">
+          <h5>{role}</h5>
+          <h3>{name}</h3>
+          <p>{bio}</p>
+        </div>
+      </article>
     </section>
   );
 };
--- src/pages/technology/Technology.jsx
+++ src/pages/technology/Technology.jsx
@@ -1,8 +1,50 @@
+import { useState } from "react";
 import data from "../../../data.json";
-const technology = data.technology;
+import "./Technology.scss";
 
 const Technology = () => {
-  return <div>Technology</div>;
+  const [activeIndex, setActiveIndex] = useState(0);
+  const technology = data.technology;
+  const activeTech = technology[activeIndex];
+  const { name, images, description } = activeTech;
+  return (
+    <section className="technology">
+      <h2>
+        <span>03</span> <span>Space launch 101</span>
+      </h2>
+
+      <article>
+        <div>
+          <img src={images.portrait} alt="" />
+          <img src={images.landscape} alt="" />
+        </div>
+
+        <nav>
+          <button
+            className={`link ${activeIndex === 0 ? "active" : ""}`}
+            onClick={() => setActiveIndex(0)}
+          >
+            1
+          </button>
+          <button
+            className={`link ${activeIndex === 1 ? "active" : ""}`}
+            onClick={() => setActiveIndex(1)}
+          >
+            2
+          </button>
+          <button
+            className={`link ${activeIndex === 2 ? "active" : ""}`}
+            onClick={() => setActiveIndex(2)}
+          >
+            3
+          </button>
+        </nav>
+        <h4>The terminology</h4>
+        <h5>{name}</h5>
+        <p>{description}</p>
+      </article>
+    </section>
+  );
 };
 
 export default Technology;
