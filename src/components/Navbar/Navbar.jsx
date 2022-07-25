ebb6a43d75d322c1328d936037c6a5394a13d0e4
AbdulrasheedAdediran <abdrasheed1103@gmail.com>
2022-07-25 22:09:37 +0100

    navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,41 +1,66 @@
+import { useState } from "react";
 import { NavLink, Link } from "react-router-dom";
 import logo from "../../../assets/shared/logo.svg";
+import closeIcon from "../../../assets/shared/icon-close.svg";
+import menuIcon from "../../../assets/shared/icon-hamburger.svg";
 import "./Navbar.scss";
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <section>
-      <div>
+    <section className="navbar">
+      <div className="logo">
         <Link to="/">
           <img src={logo} alt="Space tourism logo" />
         </Link>
       </div>
 
-      <nav>
+      <nav className={`nav__links ${isOpen ? "show" : ""}`}>
         <NavLink
           to="/"
-          className={({ isActive }) => (isActive ? "link active" : "link")}
+          className={({ isActive }) => `link ${isActive ? "active" : ""}`}
         >
-          <span>00{""}</span> <span>Home</span>
+          <span>00</span>
+          <span>Home</span>
         </NavLink>
         <NavLink
           to="/destination"
-          className={({ isActive }) => (isActive ? "link active" : "link")}
+          className={({ isActive }) => `link ${isActive ? "active" : ""}`}
         >
-          <span>01{""}</span> <span>Destination</span>
+          <span>01</span>
+          <span>Destination</span>
         </NavLink>
         <NavLink
           to="/crew"
-          className={({ isActive }) => (isActive ? "link active" : "link")}
+          className={({ isActive }) => `link ${isActive ? "active" : ""}`}
         >
-          <span>02{""}</span> <span>Crew</span>
+          <span>02</span>
+          <span>Crew</span>
         </NavLink>
         <NavLink
           to="/technology"
-          className={({ isActive }) => (isActive ? "link active" : "link")}
+          className={({ isActive }) => `link ${isActive ? "active" : ""}`}
         >
-          <span>03{""}</span> <span>Technology</span>
+          <span>03</span>
+          <span>Technology</span>
         </NavLink>
       </nav>
+
+      <button className="menu-btn" onClick={toggleMenu}>
+        <img
+          src={menuIcon}
+          alt=""
+          className={`menu-icon ${isOpen ? "hide" : "show"}`}
+        />
+        <img
+          src={closeIcon}
+          alt=""
+          className={`menu-icon ${isOpen ? "show" : "hide"}`}
+        />
+      </button>
     </section>
   );
 };
--- src/pages/SharedLayout.jsx
+++ src/pages/SharedLayout.jsx
@@ -3,7 +3,8 @@ import Navbar from "../components/NavBar/Navbar";
 import bgImage from "../../assets/home/background-home-desktop.jpg";
 const SharedLayout = () => {
   return (
-    <section style={{ backgroundImage: `url(${bgImage})` }}>
+    // <section style={{ backgroundImage: `url(${bgImage})` }}>
+    <section>
       <Navbar />
       <Outlet />
     </section>
