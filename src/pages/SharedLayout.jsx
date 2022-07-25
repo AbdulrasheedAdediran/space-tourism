b4061d64fe90c17628ccbfb78d158026b756f5d9
AbdulrasheedAdediran <abdrasheed1103@gmail.com>
2022-07-25 12:08:32 +0100

    styles

--- src/pages/SharedLayout.jsx
+++ src/pages/SharedLayout.jsx
@@ -1,12 +1,12 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/NavBar/Navbar";
-
+import bgImage from "../../assets/home/background-home-desktop.jpg";
 const SharedLayout = () => {
   return (
-    <>
+    <section style={{ backgroundImage: `url(${bgImage})` }}>
       <Navbar />
       <Outlet />
-    </>
+    </section>
   );
 };
 
--- src/pages/homepage/Homepage.jsx
+++ src/pages/homepage/Homepage.jsx
@@ -1,10 +1,11 @@
 import { Link } from "react-router-dom";
+import "./Homepage.scss";
 const Homepage = () => {
   return (
-    <section>
+    <section className="homepage">
       <div>
-        <p>So, you want to travel to</p>
-        <h1>Space</h1>
+        <h1>So, you want to travel to</h1>
+        <h2>Space</h2>
         <p>
           Let’s face it; if you want to go to space, you might as well genuinely
           go to outer space and not hover kind of on the edge of it. Well sit
