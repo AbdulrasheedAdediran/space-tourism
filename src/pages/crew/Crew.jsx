3b75fb01ae5296d3e403ae9d1a43647eb09822fa
AbdulrasheedAdediran <abdrasheed1103@gmail.com>
2022-07-29 10:58:25 +0100

    style: crew tab

--- src/pages/crew/Crew.jsx
+++ src/pages/crew/Crew.jsx
@@ -11,38 +11,39 @@ const Crew = () => {
       <h1>
         <span>02</span> <span>Meet your crew</span>
       </h1>
+      <div className="crew-wrapper">
+        <picture>
+          <source type="image/webp" srcSet={images.webp} />
+          <img src={images.png} alt={`Pictore of ${name}, a crew member`} />
+        </picture>
 
-      <picture>
-        <source type="image/webp" srcSet={images.webp} />
-        <img src={images.png} alt={`Pictore of ${name}, a crew member`} />
-      </picture>
+        <article>
+          <nav>
+            <button
+              className={`btn ${activeIndex === 0 ? "active" : ""}`}
+              onClick={() => setActiveIndex(0)}
+            ></button>
+            <button
+              className={`btn ${activeIndex === 1 ? "active" : ""}`}
+              onClick={() => setActiveIndex(1)}
+            ></button>
+            <button
+              className={`btn ${activeIndex === 2 ? "active" : ""}`}
+              onClick={() => setActiveIndex(2)}
+            ></button>
+            <button
+              className={`btn ${activeIndex === 3 ? "active" : ""}`}
+              onClick={() => setActiveIndex(3)}
+            ></button>
+          </nav>
 
-      <article>
-        <nav>
-          <button
-            className={`btn ${activeIndex === 0 ? "active" : ""}`}
-            onClick={() => setActiveIndex(0)}
-          ></button>
-          <button
-            className={`btn ${activeIndex === 1 ? "active" : ""}`}
-            onClick={() => setActiveIndex(1)}
-          ></button>
-          <button
-            className={`btn ${activeIndex === 2 ? "active" : ""}`}
-            onClick={() => setActiveIndex(2)}
-          ></button>
-          <button
-            className={`btn ${activeIndex === 3 ? "active" : ""}`}
-            onClick={() => setActiveIndex(3)}
-          ></button>
-        </nav>
-
-        <div className="crew-member">
-          <h4>{role}</h4>
-          <h2>{name}</h2>
-          <p>{bio}</p>
-        </div>
-      </article>
+          <div className="crew-member">
+            <h4>{role}</h4>
+            <h2>{name}</h2>
+            <p>{bio}</p>
+          </div>
+        </article>
+      </div>
     </section>
   );
 };
