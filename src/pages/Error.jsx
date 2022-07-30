f4608e6a8208ecdd255bb49b96ed1cffc2a2921d
AbdulrasheedAdediran <abdrasheed1103@gmail.com>
2022-07-30 07:07:40 +0100

    state animation

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,8 +1,9 @@
 import { Link } from "react-router-dom";
+import "./Error.scss";
 
 const Error = () => {
   return (
-    <div>
+    <div className="error">
       <h1>Error 404!</h1>
       <h2>The page you requested does not exist.</h2>
       <Link to="/">Back Home</Link>
--- src/pages/SharedLayout.jsx
+++ src/pages/SharedLayout.jsx
@@ -1,5 +1,5 @@
 import { Outlet } from "react-router-dom";
-import Navbar from "../components/NavBar/Navbar";
+import Navbar from "../components/navBar/Navbar";
 import { motion } from "framer-motion";
 const SharedLayout = () => {
   return (
