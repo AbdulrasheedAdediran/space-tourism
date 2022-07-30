e82785a1c8fb43491ace57f9700d42c8f15535a9
AbdulrasheedAdediran <abdrasheed1103@gmail.com>
2022-07-30 07:16:52 +0100

    state animation

--- src/pages/SharedLayout.jsx
+++ src/pages/SharedLayout.jsx
@@ -1,5 +1,5 @@
 import { Outlet } from "react-router-dom";
-import Navbar from "../components/navBar/Navbar";
+import Navbar from "../components/navbar/Navbar";
 import { motion } from "framer-motion";
 const SharedLayout = () => {
   return (
