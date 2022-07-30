cf2600d076495df31dac471a70aa0a90cfcd8974
AbdulrasheedAdediran <abdrasheed1103@gmail.com>
2022-07-30 05:15:33 +0100

    style: technology desktop

--- src/pages/technology/Technology.jsx
+++ src/pages/technology/Technology.jsx
@@ -36,30 +36,32 @@ const Technology = () => {
           />
         </picture>
 
-        <nav>
-          <button
-            className={`btn ${activeIndex === 0 ? "active" : ""}`}
-            onClick={() => setActiveIndex(0)}
-          >
-            1
-          </button>
-          <button
-            className={`btn ${activeIndex === 1 ? "active" : ""}`}
-            onClick={() => setActiveIndex(1)}
-          >
-            2
-          </button>
-          <button
-            className={`btn ${activeIndex === 2 ? "active" : ""}`}
-            onClick={() => setActiveIndex(2)}
-          >
-            3
-          </button>
-        </nav>
-        <div className="technology-content">
-          <h3>The terminology...</h3>
-          <h2>{name}</h2>
-          <p>{description}</p>
+        <div className="technology-wrapper">
+          <nav>
+            <button
+              className={`btn ${activeIndex === 0 ? "active" : ""}`}
+              onClick={() => setActiveIndex(0)}
+            >
+              1
+            </button>
+            <button
+              className={`btn ${activeIndex === 1 ? "active" : ""}`}
+              onClick={() => setActiveIndex(1)}
+            >
+              2
+            </button>
+            <button
+              className={`btn ${activeIndex === 2 ? "active" : ""}`}
+              onClick={() => setActiveIndex(2)}
+            >
+              3
+            </button>
+          </nav>
+          <div className="technology-content">
+            <h3>The terminology...</h3>
+            <h2>{name}</h2>
+            <p>{description}</p>
+          </div>
         </div>
       </article>
     </section>
