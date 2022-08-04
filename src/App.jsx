b2826d881cc94024e608afc41e605ccf1dcd00a0
AbdulrasheedAdediran <abdrasheed1103@gmail.com>
2022-08-04 15:58:46 +0100

    fix: minor bugs

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
-import { useState, useLayoutEffect } from "react";
+import * as React from "react";
+import { useState, useLayoutEffect, Suspense } from "react";
 import { Routes, Route, useLocation } from "react-router-dom";
 import { AnimatePresence } from "framer-motion";
-import Homepage from "./pages/homepage/Homepage";
+import Navbar from "./components/navbar/Navbar";
 import "./App.scss";
-import Crew from "./pages/crew/Crew";
-import Technology from "./pages/technology/Technology";
-import Destination from "./pages/destination/Destination";
-import SharedLayout from "./pages/SharedLayout";
-import Error from "./pages/Error";
+const Homepage = React.lazy(() => import("./pages/homepage/Homepage"));
+const Crew = React.lazy(() => import("./pages/crew/Crew"));
+const Technology = React.lazy(() => import("./pages/technology/Technology"));
+const Destination = React.lazy(() => import("./pages/destination/Destination"));
+const SharedLayout = React.lazy(() => import("./pages/SharedLayout"));
+const Error = React.lazy(() => import("./pages/Error"));
 
 const App = () => {
   const [activePage, setActivePage] = useState("");
@@ -19,15 +21,17 @@ const App = () => {
   return (
     <AnimatePresence exitBeforeEnter>
       <div className={`main-bg ${activePage}`}>
-        <Routes key={location.pathname} location={location}>
-          <Route path="/" exact element={<SharedLayout />}>
-            <Route index element={<Homepage />} />
-            <Route path="/destination" exact element={<Destination />} />
-            <Route path="/crew" exact element={<Crew />} />
-            <Route path="/technology" exact element={<Technology />} />
-          </Route>
-          <Route path="*" element={<Error />} />
-        </Routes>
+        <Suspense fallback={<Navbar />}>
+          <Routes key={location.pathname} location={location}>
+            <Route path="/" exact element={<SharedLayout />}>
+              <Route index element={<Homepage />} />
+              <Route path="/destination" exact element={<Destination />} />
+              <Route path="/crew" exact element={<Crew />} />
+              <Route path="/technology" exact element={<Technology />} />
+            </Route>
+            <Route path="*" element={<Error />} />
+          </Routes>
+        </Suspense>
       </div>
     </AnimatePresence>
   );
